2fe3a70a12cac070d90385744582b27d2d3c70ae
crew1274 <crew1274@yahoo.com.tw>
2017-04-09 05:02:52 +0800

    update auth

---
+++ resources/lang/zh/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密碼最少6個字元!',
+    'reset' => '你的密碼已經重設!',
+    'sent' => 'We have e-mailed your password reset link!',
+    'token' => 'This password reset token is invalid.',
+    'user' => "我們沒有紀錄此email的帳號!.",
+
+];
